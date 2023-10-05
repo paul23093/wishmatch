716dbf1389db7e50f869473fdb0d195e6f20db99
psemenov <pavel.semenov@exness.com>
2023-10-05 11:40:04 +0300

    Minor fixes

--- static/index.js
+++ static/index.js
@@ -48,10 +48,10 @@ async function load() {
     let titleText = initData.user.first_name;
     if (["group", "supergroup"].includes(chat_type)) {
         titleText = 'Group wishes';
-    }
-    titleText += " - " + users.length + " user";
-    if (users.length > 1) {
-        titleText += "s";
+        titleText += " - " + users.length + " user";
+        if (users.length > 1) {
+            titleText += "s";
+        }
     }
     document.getElementById("title").textContent = titleText;
 
