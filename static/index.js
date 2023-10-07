6eda4d9c4fbca25ecb355aa1705ecb07854f951d
psemenov <pavel.semenov@exness.com>
2023-10-07 11:24:07 +0300

    Minor changes

--- static/index.js
+++ static/index.js
@@ -47,7 +47,7 @@ async function load() {
     }
     document.getElementById("title").textContent = titleText;
 
-    if (users.length === 1 && users[0].tg_user_id === initData.user.id) {
+    if (user_id !== chat_id) {
         for (let i = 0; i < wishes.length; i++) {
             let wish = wishes[i];
             let card = document.createElement("div");
