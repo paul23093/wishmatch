c79e747922f162edef67fd1243058f503bd4c44f
psemenov <pavel.semenov@exness.com>
2023-10-08 19:35:37 +0300

    Minor changes

--- app.py
+++ app.py
@@ -197,6 +197,7 @@ async def get_wish(request: Request):
         ;
         """)
         data = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+        print(data)
 
         return json.dumps({"status": "ok", "data": data})
 
