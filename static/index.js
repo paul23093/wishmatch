df725f2c1cafe5493a4165b540a33b202d11becf
psemenov <pavel.semenov@exness.com>
2023-10-10 20:03:37 +0300

    Minor changes

--- static/index.js
+++ static/index.js
@@ -464,7 +464,7 @@ async function load_new_wish() {
         window.location.replace(document.referrer);
     });
     Telegram.WebApp.BackButton.show();
-    let initialHeight = window.innerHeight;
+    let keyboardHeight = 250;
     let inputs = Array.prototype.slice.call(document.querySelectorAll("input[data-index]"));
     let inputCount = inputs.reduce((prev, curr) => curr > prev ? curr.getAttribute("data-index") : prev.getAttribute("data-index"));
     document.querySelectorAll("div[class='input'].input").forEach(function (el) {
@@ -484,19 +484,14 @@ async function load_new_wish() {
             }
         });
         el.addEventListener("focusin", function (e) {
-            checkInput(e);
-        });
-
-        el.addEventListener("touchend", function (e) {
-            const currentHeight = window.innerHeight;
-            let keyboardHeight = Math.abs(currentHeight - initialHeight);
             document.getElementById("title").textContent = document.documentElement.clientHeight.toString();
             const inputFieldRect = el.getBoundingClientRect();
-            document.body.style.height = (initialHeight + keyboardHeight).toString() + "px";
+            document.body.style.height = (window.innerHeight + keyboardHeight).toString() + "px";
             window.scrollTo({
-                top: inputFieldRect.bottom - initialHeight + keyboardHeight,
+                top: inputFieldRect.bottom - window.innerHeight + keyboardHeight,
                 behavior: 'smooth'
             });
+            checkInput(e);
         });
 
 
