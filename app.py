c22cb05bedbcebfcd5a312f390d4ad372ea60ee5
psemenov <pavel.semenov@exness.com>
2023-11-03 09:08:54 +0200

    Minor changes

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ async def get_chat_info(request: Request):
             ;
         """)
         data = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()][0]
-        return json.dumps({"status": "ok", "data": data})
+        return json.dumps({"status": "ok", "data": json.dumps(data)})
 
 
 @app.post("/get_wishes")
